3de36c71f346fa7cff3d69542b69affd551c2aa7
PC & Hardware <pcandhardware55@gmail.com>
2024-04-29 16:45:44 +0530

    check and debugging

--- daily.py
+++ daily.py
@@ -3,14 +3,14 @@ import bal
 import json 
 def check():
     now = time()
-    with open("data/lastdaily.json") as file:
+    with open("./data/lastdaily.json") as file:
         data = json.load(file)
     last = data["lastdaily"]
 
     if round(now-last) >= 86400 and round(now-last) < 2*86400:
         ''' ADDS THE MONI'''
         bal.add((100000+1000*data["streak"]),of="pocket")
-        with open("data/lastdaily.json","wt") as file1:
+        with open("./data/lastdaily.json","wt") as file1:
             json.dump({
                 "lastdaily":now,
                 "streak":data["streak"]+1
@@ -20,13 +20,13 @@ def check():
     
     elif round(now-last) > 2*86400:
         ''' ADDS THE MONI'''
-        with open("data/lastdaily.json","wt") as fiel:
+        with open("./data/lastdaily.json","wt") as fiel:
             data["streak"] = 0
             json.dump(data,fiel)
-        with open("data/lastdaily.json") as jdj:
+        with open("./data/lastdaily.json") as jdj:
             data = json.load(jdj)
         bal.add(100000+1000*data["streak"],of="pocket")
-        with open("data/lastdaily.json","wt") as file1:
+        with open("./data/lastdaily.json","wt") as file1:
             json.dump({
                 "lastdaily":now,
                 "streak":data["streak"]+1
--- log.py
+++ log.py
@@ -1,18 +1,19 @@
+import json
 def logcheck():
     try:
-        with open("data/user.txt") as file:
-            data = file.read()
+        with open("./data/user.json") as file:
+            data = json.load(file)
             if data:
-                return data.replace("name:","")
+                return data["name"]
             else:
                 return False 
     except FileNotFoundError:
         return False
 def login(user):
     try:
-        with open("data/user.txt",'wt+') as file:
-            file.write(f"name:{user}")
-            if file.read():
+        with open("./data/user.json",'wt+') as file:
+            json.dump({"name":user})
+            if json.load(file):
                 return True
             else:
                 return False
@@ -20,4 +21,5 @@ def login(user):
         return False
         
     
-                
+if __name__=="__main__":
+    print(logcheck())              
